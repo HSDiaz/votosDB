c8cf258ecf2a0d64903a100c797cbdc2a557c3f8
Enmanuel Guerrero <enmanuelgs002@gmail.com>
2024-05-11 21:00:13 -0400

    Login actualizado y documentos agregados

---
+++ js/login.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById('loginForm');
+const errorMessage = document.getElementById('error-message');
+
+loginForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const cedula = loginForm.cedula.value;
+	const password = loginForm.password.value;
+
+	// TO DO: implement login logic here
+	// For demonstration purposes, we'll just check if the username and password are not empty
+	if (cedula && password) {
+		alert(`Bienvenido, ${cedula}!`);
+	} else {
+		errorMessage.textContent = 'Usuario o contraseña incorrecta';
+	}
+});
