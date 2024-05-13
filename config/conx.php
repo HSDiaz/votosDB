358a64f2af07bc469d8e41f3e3e1b6c779b1d154
Enmanuel Guerrero <enmanuelgs002@gmail.com>
2024-05-12 22:53:58 -0400

    conexion a BD modificada y layout añadido (para después)

---
+++ config/conx.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "127.0.0.1";
+$username = "enmanuel";
+$password = "s-e5C4haL4re3fR";
+$database = "votos";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+echo "Conexión exitosa";
+
+?>
